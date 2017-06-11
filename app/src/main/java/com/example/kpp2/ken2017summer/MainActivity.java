f9fc4ef9860ac1b8ab137e2c8631afc52eb12420
Ken Phillip Pierre II <ken.pierre2@me.com>
2017-06-11 11:46:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kpp2/ken2017summer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kpp2.ken2017summer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/kpp2/ken2017summer/NewActivity.java
@@ -0,0 +1,17 @@
+package com.example.kpp2.ken2017summer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Kpp2 on 6/11/17.
+ */
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
